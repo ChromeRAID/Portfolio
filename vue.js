531d9f0b3de7c4b02a34d5b1649153a7eb58924c
Kesavan Shan <kns_8101@hotmail.com>
2020-03-14 15:21:37 -0400

    Skeleton

---
+++ vue.js
@@ -0,0 +1,20 @@
+'use strict';
+const app = new Vue({
+    el: '#app',
+    data: {
+     headings: [
+        {name: "Home", link: 'index.html'},
+         {name: "Technical Work", link: 'techwork.html'},
+         {name: "Academic Projects", link: 'academicprojects.html'},
+         {name: "Personal Projects", link: 'personalprojects.html'},
+         {name: "Traditional Work", link: 'traditionalwork.html'},
+         {name: "Resume", link: 'resume.html'},
+         {name: "Contact Me", link: 'contact.html'},
+
+     ]
+
+    },
+    methods: {
+      
+ } // end methods
+});
